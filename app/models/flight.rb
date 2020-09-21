65ab5c7b1622f563fefc9a1f41786213d8ab6806
RyanDBarnett <conner2468@gmail.com>
2020-09-21 11:48:13 -0600

    Add find passenger flight id helper method to flight model w test

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,8 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :passenger_flights
   has_many :passengers, through: :passenger_flights
+
+  def find_passenger_flight_id passenger_id
+    passenger_flights.find_by(passenger_id: passenger_id).id
+  end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -10,4 +10,18 @@ describe Flight do
     it { should have_many :passenger_flights }
     it { should have_many(:passengers).through(:passenger_flights) }
   end
+
+  describe 'instance methods' do
+    it 'find_passenger_flight' do
+      @airline = Airline.create!(name: 'Frontier')
+
+      @pas_1 = Passenger.create!(name: "Pas 1", age: 5)
+
+      @flight_1 = Flight.create!(number: 36012, airline: @airline)
+
+      @pas_flight_1 = PassengerFlight.create!(passenger: @pas_1, flight: @flight_1)
+
+      expect(@flight_1.find_passenger_flight_id(@pas_1.id)).to eq(@pas_flight_1.id)
+    end
+  end
 end
